76cee3b859f3f9b321578411ffe1688bc8adfb8a
Sammed <sammed.shantinath@iitgn.ac.in>
2017-06-19 20:16:20 +0530

    Applied footer

---
+++ js/madeby.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$("#nm").animate({bottom:'3%'}, 3000);
+	$("#nm1").animate({left:'3%'}, 3000);
+	$("#ap").animate({bottom:'3%'}, 3000);
+	$("#ap1").animate({left:'25%'}, 2000);
+	$("#sa").animate({bottom:'3%'}, 3000);
+	$("#sa1").animate({right:'34.5%'}, 2000);
+	$("#mb").animate({bottom:'3%'}, 3000);
+	$("#mb1").animate({right:'7.5%'}, 3000);
+})
