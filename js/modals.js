41e668a05e22b251a553981eae2aba5bb07a012e
Apoorv Asus ROG Gitdesktop <apoorvagni@hotmail.com>
2017-06-27 20:28:49 +0530

    modal dynamic sorta

--- js/modals.js
+++ js/modals.js
@@ -1,3 +1,11 @@
-function SubstationTracer(substation) {
-	document.getElementById(substation)
+var sub;
+function SubstationTracer(sub){
+	alert('x');
+	alert(sub);
+	document.getElementById("modal").innerHTML = '<h1 id="madar">' + sub + '</h1>' + '<a href="" class="close">&#215;</a>\
+<div id="p">\
+<form id="form_s2" action="" method="post">\
+<input class="historyinput" type="radio" name="joke" value="History" /> History\
+<input class="presentinput" type="radio" name="joke" value="Present" /> Present\
+<input class="okinput" type="submit" value="OK"></form></div>';
 }
