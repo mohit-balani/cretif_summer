e7ef772fddf7ab2d1e3605c1f740dab24a9d0457
Sammed <sammed.shantinath@iitgn.ac.in>
2017-06-29 19:45:43 +0530

    commit

---
+++ js/modals.js
@@ -0,0 +1,10 @@
+var sub;
+function SubstationTracer(sub){
+	alert(sub);
+	document.getElementById("modal").innerHTML = '<h1>' + sub + '</h1>' + '<a href="" class="close">&#215;</a>\
+<div id="p">\
+<form id="form_s2" action="" method="post">\
+<input class="historyinput" type="radio" name="joke" value="History" /> History\
+<input class="presentinput" type="radio" name="joke" value="Present" /> Present\
+<input class="okinput" type="submit" value="OK"></form></div>';
+}
