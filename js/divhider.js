2030ebceaf2e6e0b56ec3c9bb44a06d68f1ebe79
Apoorv Asus ROG Gitdesktop <apoorvagni@hotmail.com>
2017-06-29 22:13:12 +0530

    i am useless.

--- js/divhider.js
+++ js/divhider.js
@@ -1,7 +1,45 @@
-$(document).ready(function(){
-	$("#entire").hide();
-    $("button").click(function(){
-        $("p").hide(1000);
-        $("#entire").show(1000);
-    });
+var elementID;
+function toggleDivHider(elementID){
+	var element = document.getElementById(elementID);
+	if (element.style.display === 'none'){
+		unfade(elementID);
+	}
+	else {
+		fade(elementID);
+	}
+}
+function fade(elementID) {
+	var element = document.getElementById(elementID);
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+function unfade(elementID) {
+	var element = document.getElementById(elementID);
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 50);
+}
+/*
+$(window).click(function() {
+//Hide the menus if visible
 });
+
+$('#menucontainer').click(function(event){
+    event.stopPropagation();
+});
+*/
