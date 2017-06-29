0ace91a3107117108572154f5604bd8298272541
Apoorv Asus ROG Gitdesktop <apoorvagni@hotmail.com>
2017-06-29 20:04:22 +0530

    Add all safe files.

---
+++ js/madeby2.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+  $("#nm").animate({bottom:'3%'}, 3000);
+  $("#nm1").animate({left:'3%'}, 3000);
+  $("#ap").animate({bottom:'3%'}, 3000);
+  $("#ap1").animate({left:'25%'}, 2000);
+  $("#sa").animate({bottom:'3%'}, 3000);
+  $("#sa1").animate({right:'34.5%'}, 2000);
+  $("#mb").animate({bottom:'3%'}, 3000);
+  $("#mb1").animate({right:'7.5%'}, 3000);
+  var x='4,70,702';
+document.getElementById('poas1').value=x;
+var x='4,70,702';
+document.getElementById('poas2').value=x;
+var y='4,70,702.72';
+document.getElementById('pomtp').value=y;
+
+var z='4,70,';
+document.getElementById('pohs').value=z;
+var a='4,70,702.72';
+document.getElementById('pomi').value=a;
+var x='4,70,702';
+document.getElementById('voas1').value=x;
+var x='4,70,702';
+document.getElementById('voas2').value=x;
+var y='4,70,702.72';
+document.getElementById('vomtp').value=y;
+
+var z='4,70,';
+document.getElementById('vohs').value=z;
+var a='4,70,702.72';
+document.getElementById('vomi').value=a;
+var x='4,70,702';
+document.getElementById('inas1').value=x;
+var x='4,70,702';
+document.getElementById('inas2').value=x;
+var y='4,70,702.72';
+document.getElementById('inmtp').value=y;
+
+var z='4,70,';
+document.getElementById('inhs').value=z;
+var a='4,70,702.72';
+document.getElementById('inmi').value=a;
+var x='4,70,702';
+document.getElementById('fras1').value=x;
+var x='4,70,702';
+document.getElementById('fras2').value=x;
+var y='4,70,702.72';
+document.getElementById('frmtp').value=y;
+
+var z='4,70,';
+document.getElementById('frhs').value=z;
+var a='4,70,702.72';
+document.getElementById('frmi').value=a;
+
+
+})
+var sub;
+function substationTracer(sub){
+  alert(sub)
+} 
+
+
