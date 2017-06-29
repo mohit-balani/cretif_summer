50727ce58aff688e371aa38421711cb6f65b1fb1
Apoorv Asus ROG Gitdesktop <apoorvagni@hotmail.com>
2017-06-29 16:52:32 +0530

    to fix, open hidtory if seletcting history

--- js/modals.js
+++ js/modals.js
@@ -1,6 +1,6 @@
 var sub;
 function SubstationTracer(sub){
-	window.alert(sub);
+	alert(sub);
 	document.getElementById("modal").innerHTML = '<h1>' + sub + '</h1>' + '<a href="" class="close">&#215;</a>\
 <div id="p">\
 <form id="form_s2" action="" method="post">\
