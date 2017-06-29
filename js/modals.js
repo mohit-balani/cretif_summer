9d0b89a4e78e69565ee3006a9929a5089289b065
Apoorv Asus ROG Gitdesktop <apoorvagni@hotmail.com>
2017-06-29 16:45:32 +0530

    fixed merging samm

--- js/modals.js
+++ js/modals.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-function SubstationTracer()
-=======
 var sub;
 function SubstationTracer(sub){
-	alert(sub);
+	window.alert(sub);
 	document.getElementById("modal").innerHTML = '<h1>' + sub + '</h1>' + '<a href="" class="close">&#215;</a>\
 <div id="p">\
 <form id="form_s2" action="" method="post">\
@@ -11,4 +8,3 @@ function SubstationTracer(sub){
 <input class="presentinput" type="radio" name="joke" value="Present" /> Present\
 <input class="okinput" type="submit" value="OK"></form></div>';
 }
->>>>>>> samm
